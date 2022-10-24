c080d8b9cc8ac2f6ed005b53332aa703af0142ad
11707 <ethan.lubeck@vtsd.com>
2022-10-24 07:34:27 -0400

    rep challenge

---
+++ temp.java
@@ -0,0 +1,24 @@
+public class temp {
+    public static void main(String args[]){
+       // Make loop 100,97,94,91
+
+
+        for(int i = 0; i<=100; i+=3 ){
+
+             int k = Math.abs( -i +100) ;
+
+            System.out.print(k + ", ");
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
